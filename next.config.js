db07dd898506aeff39d28ffaebafab137ed51a7e
Taylor Caldwell <tcaldwell@twitter.com>
2025-04-10 16:17:42 -0700

    update config files

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: ['pbs.twimg.com'], // Allow images from X/Twitter
   },
+  // Ensure proper handling of trailing slashes
+  trailingSlash: false,
+  // Enable static optimization where possible
+  swcMinify: true,
+  // Ensure proper handling of basePath if you're using a custom domain
+  basePath: '',
 }
 
 module.exports = nextConfig 
